a6178e6a162a13fa554952b5d90eb25a9b73bda2
Takunda Madechangu <madechangu.takunda@gmail.com>
2023-11-07 15:56:17 +0200

    Changed push instructions

--- push_git.sh
+++ push_git.sh
@@ -1,4 +1,4 @@
-mysqldump -u root --password=password madondo_home_care > website/db.sql
+mysqldump -u root --password=password matthew_bros > website/db.sql
 git add .
 git commit -m "Added new changes"
 git push origin dev
